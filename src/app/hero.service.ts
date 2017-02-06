377cf6ef4e2d7ec227dcf9e4e437bd681ab67612
Chris Lowther <chris.lowther1024@gmail.com>
2017-02-06 15:37:17 +1300

    Http bit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,54 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http'
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes' //URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+              .toPromise()
+              .then(res => res.json().data as Hero[])
+              .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
-    return Promise.resolve(HEROES.find(h => h.id === id))
+    const url = `${this.heroesUrl}/${id}`
+
+    return this.http.get(url)
+              .toPromise()
+              .then(res => res.json().data as Hero)
+              .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`
+
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+              .toPromise()
+              .then(() => hero)
+              .catch(this.handleError);
+  }
+
+  create(heroName: string): Promise<Hero> {
+    return this.http.post(this.heroesUrl, JSON.stringify({name: heroName}), {headers: this.headers})
+              .toPromise()
+              .then(res => res.json().data as Hero)
+              .catch(this.handleError);
+  }
+
+  delete(heroId: number): Promise<any> {
+    const url = `${this.heroesUrl}/${heroId}`
+
+    return this.http.delete(url, {headers: this.headers})
+              .toPromise()
+              .catch(this.handleError);
   }
   // See the "Take it slow" appendix
   getHeroesSlowly(): Promise<Hero[]> {
@@ -18,5 +56,10 @@ export class HeroService {
       // Simulate server latency with 2 second delay
       setTimeout(() => resolve(this.getHeroes()), 2000);
     });
+  }
+
+  handleError(error: any): Promise<any> {
+    console.error('an error occured', error);
+    return Promise.reject(error.message || error);
   }
 }
